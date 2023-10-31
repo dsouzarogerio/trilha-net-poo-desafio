75c45b22fb8f3b9077fd306ad14007273d40e0d7
Rogerio de Souza <dsouza.rogerio@gmail.com>
2023-10-30 21:47:36 -0300

    commit inicial

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,16 +1,32 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modeloTelefone, string numeroImei, int quantidadeMemoria)
         {
-            Numero = numero;
+            Numero = formatarTelefone(numero);
             // TODO: Passar os parâmetros do construtor para as propriedades
+            modelo = modeloTelefone;
+            imei = numeroImei;
+            memoria = quantidadeMemoria;
         }
 
+        private string formatarTelefone(string numero)
+        {
+            string regexNumero = @"^(\d{2})9(\d{4,5})(\d{4})$";
+            string formato = "($1) 9$2-$3";
+            string celularFormatado = Regex.Replace(numero, regexNumero, formato);
+            return celularFormatado;
+        }
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string imei;
+        private int memoria;
+
+        public string Numero { get; set; }
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
@@ -22,5 +38,14 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void ExibirDadosTelefone()
+        {
+            Console.WriteLine("---- DADOS DO TELEFONE ----");
+            Console.WriteLine($"Nº tel.: {Numero}\n" +
+                              $"Modelo: {modelo}\n" +
+                              $"IMEI: {imei}\n" +
+                              $"Capacidade de Memória: {memoria}GB");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("11912345678", "NOKIA", "123456", 16);
+nokia.InstalarAplicativo("Instagram");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.ExibirDadosTelefone();
+
+Iphone iphone = new Iphone("31912345679", "IPHONE", "78945612", 64);
+iphone.ExibirDadosTelefone();
+iphone.InstalarAplicativo("Telegram");
+iphone.Ligar();
+iphone.ReceberLigacao();
